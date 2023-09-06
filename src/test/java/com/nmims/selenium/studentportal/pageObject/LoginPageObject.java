315dde83d8b345dc1b890ccc282b768a301c29df
amitkumrydv <amitkumrydv@gmail.com>
2023-09-06 13:56:04 +0530

    change package name

---
+++ src/test/java/com/nmims/selenium/studentportal/pageObject/LoginPageObject.java
@@ -0,0 +1,42 @@
+package com.nmims.selenium.studentportal.pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPageObject {
+
+	WebDriver driver;
+	
+	
+
+	public LoginPageObject(WebDriver driver) {
+		
+		PageFactory.initElements(driver, this); // Define as page object class
+	}
+	
+	
+    // Xpath
+	@FindBy(xpath = "//h2[contains(text(),'WELCOME TO NGASCE')]")
+	@CacheLookup
+	protected WebElement loginPageTitle;
+
+	@FindBy(id = "userId")
+	@CacheLookup
+	protected WebElement userId;
+
+	@FindBy(name = "password")
+	@CacheLookup
+	protected WebElement pswd;
+
+	@FindBy(xpath = "//button[contains(text(), 'LOGIN')]")
+	@CacheLookup
+	protected WebElement loginButton;
+	
+	
+
+
+
+}
---
+++ src/test/java/com/nmims/selenium/studentportal/pageObjectMethod/LoginPageObjectMethod.java
@@ -0,0 +1,37 @@
+package com.nmims.selenium.studentportal.pageObjectMethod;
+
+import org.openqa.selenium.WebDriver;
+
+import com.nmims.selenium.studentportal.pageObject.LoginPageObject;
+
+public class LoginPageObjectMethod extends LoginPageObject{
+	
+
+	
+	public LoginPageObjectMethod(WebDriver driver) {
+		super(driver);
+	
+	}
+
+
+	public String verifyTitle() {
+		return loginPageTitle.getText();
+	}
+	
+	
+	public void setUserName(String userName) {
+		userId.clear();
+		userId.sendKeys(userName);
+	}
+	
+	public void setPassword(String password) {
+		pswd.clear();
+		pswd.sendKeys(password);
+	}
+	
+	public void clickSubmit() {
+
+		loginButton.click();
+	}
+
+}
