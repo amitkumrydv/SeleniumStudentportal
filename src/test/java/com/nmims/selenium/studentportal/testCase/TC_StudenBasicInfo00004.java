ee98790314df76aec48abc8563d98f8496092b06
amitkumrydv <amitkumrydv@gmail.com>
2023-09-18 21:29:13 +0530

    student details issues resolve

--- src/test/java/com/nmims/selenium/studentportal/testCase/TC_StudenBasicInfo00004.java
+++ src/test/java/com/nmims/selenium/studentportal/testCase/TC_StudenBasicInfo00004.java
@@ -38,12 +38,15 @@ public class TC_StudenBasicInfo00004 extends BasClass {
 
 		StudaentDetail excelReader = new StudaentDetail();
 		UserDtailsPageObjectMethod webElementFetcher = new UserDtailsPageObjectMethod();
-
+try {
 		List<String> expectedOptions = excelReader.studentDetailExcel(path, "Details");
 		logger.info(expectedOptions);
+		
 		List<String> actualTextValues = webElementFetcher.fetchTextValuesFromUI(driver,"//ul[contains(@class,'student-info-list')]//li");
 		logger.info("Header Xpath pass " + actualTextValues);
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 		if (expectedOptions.containsAll(actualTextValues)) {
 			logger.info("Student data is matched from the Excel");
 			Assert.assertEquals(actualTextValues, expectedOptions, "Excel Data matched from the UI");
@@ -52,6 +55,10 @@ public class TC_StudenBasicInfo00004 extends BasClass {
 			captureScreen(driver, "loginTest");
 			Assert.assertNotSame(expectedOptions, actualTextValues);
 		}
+
+	}catch (Exception e) {
+		e.printStackTrace();
+	}
 
 	}
 
