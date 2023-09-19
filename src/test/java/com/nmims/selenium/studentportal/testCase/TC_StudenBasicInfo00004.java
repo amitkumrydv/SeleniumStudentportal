b6a4eb822a3d8be46efbc763de2c789fe1a604fe
amitkumrydv <amitkumrydv@gmail.com>
2023-09-19 08:20:43 +0530

    conflict

--- src/test/java/com/nmims/selenium/studentportal/testCase/TC_StudenBasicInfo00004.java
+++ src/test/java/com/nmims/selenium/studentportal/testCase/TC_StudenBasicInfo00004.java
@@ -38,24 +38,22 @@ public class TC_StudenBasicInfo00004 extends BasClass {
 
 		StudaentDetail excelReader = new StudaentDetail();
 		UserDtailsPageObjectMethod webElementFetcher = new UserDtailsPageObjectMethod();
-<<<<<<< HEAD
+
 try {
 		List<String> expectedOptions = excelReader.studentDetailExcel(path, "Details");
 		logger.info(expectedOptions);
-=======
 
->>>>>>> developer
 		
 		List<String> actualTextValues = webElementFetcher.fetchTextValuesFromUI(driver,"//ul[contains(@class,'student-info-list')]//li");
 		logger.info("Header Xpath pass " + actualTextValues);
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-<<<<<<< HEAD
+
 		
 		if (expectedOptions.containsAll(actualTextValues)) {
 			logger.info("Student data is matched from the Excel");
 			Assert.assertEquals(actualTextValues, expectedOptions, "Excel Data matched from the UI");
-=======
+
 		List<String> expectedOptions = excelReader.studentDetailExcel(path, "Details");
 		logger.info(expectedOptions);
 		
@@ -66,7 +64,7 @@ try {
 			
 			logger.info("Excel Data matched from the UI");
 			Assert.assertEquals(actualTextValues, expectedOptions);
->>>>>>> developer
+
 		} else {
 			logger.info("Student data is not matched from the Excel");
 			captureScreen(driver, "loginTest");
