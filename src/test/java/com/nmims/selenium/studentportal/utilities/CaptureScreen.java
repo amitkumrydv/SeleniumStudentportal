e1acb12a0b8da2513ab198a876171178073421f9
amitkumrydv <98074991+amitkumrydv@users.noreply.github.com>
2024-05-08 16:10:22 +0530

    create same branch for screen shorts

--- src/test/java/com/nmims/selenium/studentportal/utilities/CaptureScreen.java
+++ src/test/java/com/nmims/selenium/studentportal/utilities/CaptureScreen.java
@@ -1,43 +1,88 @@
 package com.nmims.selenium.studentportal.utilities;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.nmims.selenium.studentportal.baseClass.BaseClass;
 
-public class CaptureScreen extends BaseClass{
-	
-	//private WebDriver driver;
+
 	
 	
-	public CaptureScreen(WebDriver driver){
+	public class CaptureScreen extends BaseClass{
 		
-		this.driver=driver;
-	}
-	
-	
-public void captureFullScreen(String tname) throws IOException   {
-	
-	if (driver == null) {
-        System.err.println("WebDriver is not initialized.");
-        return;
-    }
-	
+		//private WebDriver driver;
 		
-		TakesScreenshot ts=(TakesScreenshot) driver;
-		File source =ts.getScreenshotAs(OutputType.FILE);
-		File target=new File(System.getProperty("user.dir") + "/Screenshots/" + tname +".png" );
+		
+		public CaptureScreen(WebDriver driver){
+			
+			this.driver=driver;
+		}
+	
+	
+	
+
+	public void captureFullScreen(String tname) throws IOException {
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File target = new File(System.getProperty("user.dir") + "/Screenshots/" + tname + ".png");
 		FileUtils.copyFile(source, target);
-		
+
 		System.out.println("Screenshot taken");
-		
+
 	}
 
+	public static void takeScreenshotUpToFooter(String tname) {
+		try {
+			
+			
+			
+			
 
+			// Take a screenshot of the entire page
+			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+			// Read the screenshot as BufferedImage
+			BufferedImage fullImg = ImageIO.read(screenshot);
+
+			// Crop the screenshot up to the footer
+//			BufferedImage footerScreenshot = fullImg.getSubimage(0, 0, fullImg.getWidth(),
+//					fullImg.getHeight() - footerHeight);
+			
+			
+			// Combine the two images
+            BufferedImage combinedImg = new BufferedImage(fullImg.getWidth(), fullImg.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            combinedImg.createGraphics().drawImage(fullImg, 0, 0, null);
+           // combinedImg.createGraphics().drawImage(fullImg, 0, scrollHeight, null);
+			
+            // Take the screenshot with timestamp
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+            String timestamp = dateFormat.format(new Date());
+			
+            System.out.println("timestamp--> "+timestamp);
+
+			// Save the cropped screenshot
+			String filePath = System.getProperty("user.dir") + "/AllScreenshots/"+timestamp+" " + tname + ".png";
+			ImageIO.write(combinedImg, "png", new File(filePath));
+
+			System.out.println("Screenshot saved successfully at: " + filePath);
+		} catch (IOException e) {
+			System.out.println("Failed to capture screenshot: " + e.getMessage());
+		}
+	}
+
+	
 
 }
